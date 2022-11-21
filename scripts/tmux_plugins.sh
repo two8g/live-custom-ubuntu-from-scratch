cce39ee0274d82a581a2f02eb1bf17c4b3f1adb5
two8g <305120985@qq.com>
2022-11-21 10:44:03 +0800

    add tmux plugins

---
+++ scripts/tmux_plugins.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/tmux-plugins/tmux-sensible ~/.tmux/plugins/tmux-sensible
+git clone https://github.com/tmux-plugins/tmux-yank ~/.tmux/plugins/tmux-yank
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
